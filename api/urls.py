afc26646280f8683c0f4fa800ff5d2422edbcd90
Ryan <ryansheikh003@gmail.com>
2024-05-17 13:37:08 -0600

    Replacing django framework with FastAPI, initialized a FastAPI project and tested a simple get request

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from api.views import index, greet, schedule
-urlpatterns=[
-    path("", index),
-    path("greet/", greet, name='greet' ),
-    path('schedule/', schedule, name='schedule')
-]
