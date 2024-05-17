486529721b893e9586c09b4d37f89018a3e680b3
Ryan <ryansheikh003@gmail.com>
2024-05-17 13:53:57 -0600

    feat(api): add POST request and test Json payload
    Added a new POST request to the api and tested it with the json payload, ✅ Test passed

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
 
-
+class req(BaseModel):
+    employees : list
+    shifts : list
 
 @app.get("/")
 def root():
     return{"hello": "world"}
+
+
+@app.post("/schedule")
+def getSchedule(req: req):
+    print(req)
+    return req
