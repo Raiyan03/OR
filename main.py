8ba87653bef107b7b487a428b3cf4e50a5a1868f
Ryan <ryansheikh003@gmail.com>
2024-07-26 00:08:41 -0600

    fix(route error) : fix the the route error
    There was a minor error in the last merge where an important line got deleted from which cause route error while

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from utils import generateSchTable, getSchedule
-from undistributedUtils import undistGetSchedule
+from testUtils import TestgetSchedule, TestgenerateSchTable
 
 app = FastAPI()
 
@@ -26,13 +26,26 @@ def schedule(req: Req):
     return schedule
 
 @app.post("/undistributed-schedule")
-def undist_schedule(req: Req):
+def Schedule(req: Req):
     """
     Generate Schedule Without Equal Distribution.
+
+    This endpoint generates a schedule without ensuring equal distribution of 
+    shifts among employees. It focuses on covering all shifts based on employee 
+    availability and preferences, without prioritizing equal shift distribution.
+
+    Args:
+        req (RequestBody): The request body containing the list of employees and shifts.
+
+    Returns:
+        dict: A dictionary containing the generated schedule.
     """
     employees = req.employees
     shifts = req.shifts
-    schedule = undistGetSchedule(employees, shifts)
+    hour_bank = req.hour_bank
+    flex_hours = req.flex_hours
+    schTable = TestgenerateSchTable(employees, shifts)
+    schedule = TestgetSchedule(employees, shifts, schTable, hour_bank, flex_hours)
     return schedule
 
 
